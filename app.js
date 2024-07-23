9b3e8b7cc4de576bc5bf4da809cf0b8b0fb979f9
spahicadis <adis@spahica.com>
2024-07-23 22:10:26 +0200

    Pseudocode for next steps

---
+++ app.js
@@ -0,0 +1,8 @@
+/*
+globalne varijable
+query selectori
+conditional rednering -> npr 2 stejta. Prva varijabla imperial koja je true odmah na pocetku ternary operator i na to DOM
+a drugi stejt metric koji je false na pocetku i onda isto ternary operator i prikazivanje
+formula izracun BMI
+
+*/
