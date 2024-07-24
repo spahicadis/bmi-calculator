04895b8c7ced12567964fa50dd3d8240cba8de67
spahicadis <adis@spahica.com>
2024-07-24 18:26:59 +0200

    Javascript conditional rendering

--- app.js
+++ app.js
@@ -6,3 +6,54 @@ a drugi stejt metric koji je false na pocetku i onda isto ternary operator i pri
 formula izracun BMI
 
 */
+
+let sex = null;
+let years = null;
+let weight = null;
+let height = null;
+let conditionalRendering1 = true;
+let conditionalRendering2 = null;
+
+const sexInfo = document.querySelectorAll('.sex');
+const yearsInfo = document.querySelector('.years');
+const weightInfo = document.querySelector('.weight');
+const heightInfo = document.querySelector('.height');
+const actionCalulcate = document.querySelector('.action')
+const calculatorBox = document.querySelector('.calculator-box');
+const statusMode1 = document.querySelector('.mode1');
+const statusMode2 = document.querySelector('.mode2');
+const conditionalRenderingMode2 = document.querySelectorAll('.conditionMode2')
+const conditionalRenderingMode1 = document.querySelectorAll('.conditionMode1')
+console.log(conditionalRenderingMode2);
+
+
+
+
+
+statusMode1.addEventListener('change', function() {
+ if(statusMode1.checked == true) {
+  conditionalRenderingMode2.forEach((element) => {
+    element.style.display = 'block';
+  })
+  conditionalRenderingMode1.forEach((element) => {
+    element.style.display = 'none';
+  })
+ } else if (statusMode1.checked == false ) {
+  conditionalRenderingMode2.forEach((element) => {
+    element.style.display = 'none';
+  })
+  conditionalRenderingMode1.forEach((element) => {
+    element.style.display = 'block';
+  })
+ }
+})
+
+statusMode2.addEventListener('change', function() {
+  console.log(`test`);
+})
+
+
+
+
+
+
